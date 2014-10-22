69517e3942eb4a16ec4d71b895944baf3855bda9
Adam Matan <adam@vioozer.com>
2014-10-23 00:04:27 +0300

    First working version.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    author = 'Adam Matan',
+    author_email = 'adam@matan.name',
+    description = 'A python script for displaying aligned comments',
+    license = 'MIT',
+    name = 'comment',
+    py_modules = ['comment'],
+    scripts = ['comment'],
+    url = 'https://github.com/adamatan/comment-builder',
+    version = '0.1',
+)
